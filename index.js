d28d79ff8fcfcdaefb2bfa7dc60fd26dd99fd791
Fahad <fahadsaadalzahrani8@gmail.com>
2023-08-07 18:51:04 +0300

    finshed my homework

---
+++ index.js
@@ -0,0 +1,17 @@
+// first task
+let Age = 14
+console.log(Age)
+const Name = ("Fahad Saad")
+console.log(Name)
+// second Task
+// it doesnt work cause const var do not change no matter what
+// third Task
+{
+var mytask = ("To print these out of there block they need to be a var instead of const/let")
+}
+{
+var my3task = ("i need to place this in blocks")
+}
+console.log(mytask)
+console.log(my3task)
+// it only work when i made them into var but when it was a const/let they give me a error
